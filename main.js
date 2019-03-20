b218f14f69d09e6c8933e1c9f6a938ac2a8a9119
tmohammad78 <mohammadtaheri506@gmail.com>
2019-03-20 15:56:32 -0700

    compelet nav

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
     $('.navigation__button').on("click",()=>{
         $('.navigation__icon').toggleClass("navigation__icon-isactive");
-        $('.navigation__nav').toggle();
-        $('.navigation__background').toggle().css({
-            transition:"all 4.5s"
-        });
+        $('.navigation__nav').toggleClass("navigation__nav-isactive");
+        $('.navigation__background').toggle();
+        $('.navigation__background').toggleClass("navigation__background-isactive");
     });
 });
