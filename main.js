a315cb4496b6ada474fe325c3c20c2c48f40b69a
tmohammad78 <mohammadtaheri506@gmail.com>
2019-03-21 02:18:48 -0700

    create popup

--- main.js
+++ main.js
@@ -5,4 +5,7 @@ $(document).ready(function(){
         $('.navigation__background').toggle();
         $('.navigation__background').toggleClass("navigation__background-isactive");
     });
+    $('#popup').on("click",()=>{
+        $(;)
+    });
 });
